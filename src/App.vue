d014c99133eb8ef94491f64fa714ae7aef30901f
Anna Hetsman <125794946+anna-gt@users.noreply.github.com>
2024-08-02 20:51:03 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<div class="post">
+			<div><strong>Название:</strong>Пост о JavaScript</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			likes: 0,
+			dislikes: 0 
+		}
+	},
+	methods: {
+		addLike() {
+			this.likes += 1;
+		},
+		addDislike() {
+			this.dislikes += 1;
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
