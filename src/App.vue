34e0172981f42603fbf25a51aa536d99a9fe8f98
Anna Hetsman <125794946+anna-gt@users.noreply.github.com>
2024-08-03 13:20:01 +0300

    fix config; refactor: make decomposition

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,49 @@
 <template>
-	<div>
-		<div class="post">
-			<div><strong>Название:</strong>Пост о JavaScript</div>
-		</div>
+  <div class="app">
+		<post-form
+			@create="createPost"
+		/>
+		<post-list 
+			:posts="posts"
+		/>
 	</div>
 </template>
 
 <script>
+import PostForm from './components/PostForm.vue';
+import PostList from './components/PostList.vue';
+
 export default {
-	data() {
-		return {
-			likes: 0,
-			dislikes: 0 
-		}
+	components: {
+		PostForm, PostList
 	},
-	methods: {
-		addLike() {
-			this.likes += 1;
-		},
-		addDislike() {
-			this.dislikes += 1;
-		}
-	}
-}
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "JavaScript", body: "Описание поста" },
+        { id: 2, title: "JavaScript 2", body: "Описание поста 2" },
+        { id: 3, title: "JavaScript 3", body: "Описание поста 3" },
+        { id: 4, title: "JavaScript 4", body: "Описание поста 4" },
+      ],
+    };
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+    },
+  },
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  margin-top: 15px;
+}
 
+.app {
+  padding: 20px;
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+// })
+
+const { defineConfig } = require("@vue/cli-service");
+const webpack = require("webpack");
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: JSON.stringify(true), // или false, в зависимости от того, используете ли вы Options API
+        __VUE_PROD_DEVTOOLS__: JSON.stringify(false), // отключение devtools в продакшн
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false), // установка флага, чтобы устранить предупреждение
+      }),
+    ],
+  },
+});
